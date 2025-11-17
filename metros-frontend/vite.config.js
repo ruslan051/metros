2fc79042067581025a5418f71e2f64d0f5752f12
ruslan051 <galatipov@yandex.ru>
2025-11-17 21:57:43 +0300

    а

--- metros-frontend/vite.config.js
+++ metros-frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Минимальная конфигурация
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-  },
-  base: '/',
+  }
 })
