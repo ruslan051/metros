52da2a25cba8870a6b8fe513af8b0235e370091a
ruslan051 <galatipov@yandex.ru>
2025-11-18 16:13:26 +0300

    f

--- metros-frontend/src/main.jsx
+++ metros-frontend/src/main.jsx
@@ -1,15 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import { AppConfig } from './AppConfig.jsx'
-import './App.css'
+import { App } from './App.jsx'
+import './index.css'
 
-console.log('🚀 main.jsx загружен успешно!')
-
-const container = document.getElementById('root');
-if (!container) {
-  console.error('❌ Контейнер #root не найден!')
-} else {
-  console.log('✅ Контейнер #root найден')
-}
-
-const root = createRoot(container);
-root.render(<AppConfig />);
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
